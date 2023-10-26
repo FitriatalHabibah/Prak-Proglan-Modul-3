06a0def4f6e9b970b56ded7e6799fee0f12d372a
FitriatalHabibah <haritsatulh@gmail.com>
2023-10-26 17:04:48 +0700

    second commit

--- Main.java
+++ Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+
+        public static void main(String[] args) {
+            System.out.println("Halo Semuanya Apa Kabar");
+            System.out.println("Sehat Semuanya kaaan???");
+        }
+    }
+
